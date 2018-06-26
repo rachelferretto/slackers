5110eabbe71fe367cf9eb7e94fc613c975bf5f81
Daniel Tsui <tsui.daniel@gmail.com>
2018-06-26 21:28:18 +1000

    oh danny boy

---
+++ 01-javascript/speedCoding/app.js
@@ -0,0 +1,27 @@
+
+var printSong = function() {
+
+  for (var i = 0; i < 4; i++) {
+    console.log('I get knocked down, but I get up again')
+    console.log('You are never gonna keep me down')
+  }
+
+  var drinks = 'whiskey vodka lager cider'.split(' ');
+
+  for (var i = 0; i < drinks.length; i++) {
+    console.log('he drinks a ' + drinks[i] + ' drink');
+  }
+
+  console.log('He sings the songs that remind him of the good times')
+  console.log('He sings the songs that remind him of the best times')
+  console.log('(oh ' + 'danny boy, '.repeat(3).slice(0,-2) + ')')
+
+  for (var i = 0; i < 4; i++) {
+    console.log('I get knocked down, but I get up again')
+    console.log('You are never gonna keep me down')
+  }
+
+}
+
+
+
